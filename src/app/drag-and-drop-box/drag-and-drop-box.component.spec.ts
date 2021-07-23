3362dc595345367a4e740dbee7fac87524fca7bb
Borewit <borewit@users.noreply.github.com>
2021-07-23 18:44:21 +0200

    Update to Angular v10

--- src/app/drag-and-drop-box/drag-and-drop-box.component.spec.ts
+++ src/app/drag-and-drop-box/drag-and-drop-box.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DragAndDropBoxComponent } from './drag-and-drop-box.component';
 
@@ -6,7 +6,7 @@ describe('DragAndDropBoxComponent', () => {
   let component: DragAndDropBoxComponent;
   let fixture: ComponentFixture<DragAndDropBoxComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DragAndDropBoxComponent ]
     })
