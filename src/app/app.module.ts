e48bda215cae68132ae95b3f8e353e986ae9ac19
Borewit <Borewit@users.noreply.github.com>
2024-07-08 22:06:47 +0200

    Update to Angular v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DragAndDropBoxComponent } from './drag-and-drop-box/drag-and-drop-box.component';
 import { NgBytesPipeModule } from 'angular-pipes';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    DragAndDropBoxComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    NgBytesPipeModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        DragAndDropBoxComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        NgBytesPipeModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
